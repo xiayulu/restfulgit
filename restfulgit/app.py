58231f8411ccb25d1d9d0b3f5b95258f0f94acde
xiayulu <zuiaiqiansen@163.com>
2021-01-10 16:23:55 +0800

    add dockerfile for deploy

--- restfulgit/app.py
+++ restfulgit/app.py
@@ -6,11 +6,11 @@ from restfulgit.app_factory import create_app
 
 application = create_app()
 LOADED_CONFIG = application.config.from_envvar('RESTFULGIT_CONFIG', silent=True)
-LOADED_CONFIG = LOADED_CONFIG or application.config.from_pyfile('/etc/restfulgit.conf.py', silent=True)
+LOADED_CONFIG = LOADED_CONFIG or application.config.from_pyfile('../config.py', silent=True)
 if not LOADED_CONFIG:
     raise SystemExit("Failed to load any RestfulGit configuration!")
 
 
 if __name__ == '__main__':
     application.debug = True
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port=80)
